629c450e3031e63d8aeacc2fbf719e1536e84db2
Mst Rinky Akter <mstrinkya@gmail.com>
2023-04-10 14:25:16 +0600

    shared balance update function

---
+++ withdraw.js
@@ -0,0 +1,10 @@
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+    // console.log('withdraw button clicked');
+    const newWithdrawAmount = getInputFieldValueById('withdraw-field');
+    const previousWithdrawTotal = getTextElementValueById('withdraw-total');
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    setTextElementValueById('withdraw-total', newWithdrawTotal);
+    const previousBalanceTotal = getTextElementValueById('balance-total');
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    setTextElementValueById('balance-total',newBalanceTotal);
+})
