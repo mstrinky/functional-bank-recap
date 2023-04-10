a955a840970ad71a9f9e02b3a3c2f77405ec7ba5
Mst Rinky Akter <mstrinkya@gmail.com>
2023-04-10 12:38:37 +0600

    get and set element value using a function

--- deposit.js
+++ deposit.js
@@ -6,7 +6,30 @@ function getInputFieldValueById(inputFieldId){
     return inputFieldValue;
 }
 
+function getTextElementValueById(elemnetId){
+    const element = document.getElementById(elemnetId);
+    const elementValueString = element.innerText;
+    const elementValue = parseFloat(elementValueString);
+    return elementValue ;
+}
+
+function setTextElementValueById(elementId , newValue){
+    const textElement =document.getElementById(elementId);
+    textElement.innerText = newValue;
+}
+
 document.getElementById('btn-deposit').addEventListener('click',function(){
+
 const newDepositAmount = getInputFieldValueById('deposit-field');
-console.log(newDepositAmount);
+// console.log(newDepositAmount);
+
+const previousDepositTotal = getTextElementValueById('deposit-total');
+// console.log(previousDepositTotal);
+
+const newDepositTotal = previousDepositTotal + newDepositAmount;
+setTextElementValueById('deposit-total', newDepositTotal);
+
+const previousBalanceTotal = getTextElementValueById('balance-total');
+const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+setTextElementValueById('balance-total', newBalanceTotal);
 })
